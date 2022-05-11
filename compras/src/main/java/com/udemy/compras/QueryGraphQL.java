63754db5223c8bb5a45fcd0796f1fab72e768449
MatheusPMC <matheus.pmc@hotmail.com>
2022-05-11 20:44:11 -0300

    Criando lista de clients

--- compras/src/main/java/com/udemy/compras/QueryGraphQL.java
+++ compras/src/main/java/com/udemy/compras/QueryGraphQL.java
@@ -3,6 +3,9 @@ package com.udemy.compras;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver {
 
@@ -11,6 +14,21 @@ public class QueryGraphQL implements GraphQLQueryResolver {
     // Site para testar o graphql: https://www.graphqlbin.com/v2/6RQ6TM
 
     //Liberando o playground: http://localhost:8080/playground
+
+    /* Testando as funcoes
+{
+  hello,
+  soma(a:4, b:3),
+  client{
+    name,
+    email
+  },
+  clients {
+    name,
+    email
+  }
+}
+     */
     public String hello() {
         return "Hello GraphQL";
     }
@@ -21,5 +39,13 @@ public class QueryGraphQL implements GraphQLQueryResolver {
 
     public Client client() {
         return new Client("Matheus","matheus@test.com");
+    }
+
+    public List<Client> clients() {
+        List<Client> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Client("Client " +1, "email"+i+"@test.com"));
+        }
+        return list;
     }
 }
