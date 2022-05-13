b3606f626eb1ffb93e3188120f4a3309004a28d3
MatheusPMC <matheus.pmc@hotmail.com>
2022-05-13 13:38:02 -0300

    Mutation Criando um cliente

--- compras/src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
+++ compras/src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
@@ -1,15 +1,17 @@
 package com.udemy.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.udemy.compras.Cliente;
 import com.udemy.compras.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     /*
 {
@@ -34,5 +36,11 @@ public class ClienteGraphQL implements GraphQLQueryResolver {
 
     public List<Cliente> clientes() {
         return clienteRep.findAll();
+    }
+
+    @Transactional
+    public Cliente saveCliente(Long id, String nome, String email) {
+        Cliente c = new Cliente(id, nome, email);
+        return clienteRep.save(c);
     }
 }
